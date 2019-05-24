d5967bc0eb739ba390d77b84e93e9373b0d503eb
vinyard78 <luck.ballaud@wanadoo.fr>
2019-05-24 17:03:04 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Material */
+import { 
+  MatListModule,
+  MatIconModule,
+  MatButtonModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatSortModule } from '@angular/material';
+
+/* Page */
+import { HomeComponent } from './pages/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
